b49bdadd4535a66d92c3ad4a11b8fb4ebc3d4a12
lonnalu <lonnalu@gmail.com>
2021-12-29 15:59:39 -0800

    Changes routes to async awaits and updates model to Artwork singular

--- routes/Artworks.js
+++ routes/Artworks.js
@@ -1,28 +1,38 @@
 const express = require("express");
-const Artworks = require("../models/Artworks");
+const Artwork = require("../models/Artwork");
 const router = express.Router();
+
 // Request for all art work
-router.get("/", (req, res) => {
-  Artworks.find()
-    .then((artwork) => res.json(artwork))
-    .catch((err) => res.status(400).json(`Error: ${err}`));
+router.get("/", async (req, res) => {
+  try {
+    const artworks = await Artwork.find();
+    return res.status(200).json(artworks)
+
+  } catch (err) {
+    return res.status(500).json(`Error: ${err}`);
+  }
 });
+
 // Add new Artwork
-router.post("/new", (req, res) => {
-  const newArtwork = new Artworks({
-    title: req.body.title,
-    image: req.body.image,
-  });
-  newArtwork
-    .save()
-    .then(() => res.json("New Artwork Posted"))
-    .catch((err) => res.status(400).json(`Error: ${err}`));
+router.post("/new", async (req, res) => {
+  try {
+    const newArtwork = await Artwork.create(req.body);
+    return res.status(201).json("New Artwork Posted")
+
+  } catch (err) {
+    return res.status(400).json(`Error: ${err}`);
+  }
 });
+
 // Get single Art work
-router.get("/:id", (req, res) => {
-  Artworks.findById(req.params.id)
-    .then((artwork) => res.json(artwork))
-    .catch((err) => res.status(400).json(`Error: ${err}`));
+router.get("/:id", async (req, res) => {
+  try {
+    const artwork = await Artwork.findById(req.params.id);
+    return res.status(200).json(artwork);
+
+  } catch (err) {
+    return res.status(500).json(`Error: ${err}`)
+  }
 });
 
 module.exports = router;
