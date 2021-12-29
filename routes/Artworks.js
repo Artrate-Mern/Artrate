079b265ac20b4b45435fc8f86b66d94432b9fe31
java80 <iekedede@hotmail.com>
2021-12-29 18:35:22 -0500

    changes to routes

--- routes/Artworks.js
+++ routes/Artworks.js
@@ -1,22 +1,28 @@
 const express = require("express");
-const Artwork = require("../models/Artworks");
+const Artworks = require("../models/Artworks");
 const router = express.Router();
 // Request for all art work
 router.get("/", (req, res) => {
-  Artwork.find()
+  Artworks.find()
     .then((artwork) => res.json(artwork))
     .catch((err) => res.status(400).json(`Error: ${err}`));
 });
 // Add new Artwork
 router.post("/new", (req, res) => {
-  const newArtwork = new Artwork({
+  const newArtwork = new Artworks({
     title: req.body.title,
-    image: req.body.authorname
+    image: req.body.image,
   });
   newArtwork
     .save()
     .then(() => res.json("New Artwork Posted"))
-    .catch((err) => res.status(400).json(`Error: ${err}`))
+    .catch((err) => res.status(400).json(`Error: ${err}`));
+});
+// Get single Art work
+router.get("/:id", (req, res) => {
+  Artworks.findById(req.params.id)
+    .then((artwork) => res.json(artwork))
+    .catch((err) => res.status(400).json(`Error: ${err}`));
 });
 
 module.exports = router;
