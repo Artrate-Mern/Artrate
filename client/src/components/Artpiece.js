b7000790f9b66eeb37ba06166e0f19076454bbde
lonnalu <lonnalu@gmail.com>
2021-12-30 12:34:59 -0800

    Prevents carousel from auto scrolling

--- client/src/components/Artpiece.js
+++ client/src/components/Artpiece.js
@@ -9,10 +9,8 @@ import Button from "@mui/material/Button";
 import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
 import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
 import SwipeableViews from "react-swipeable-views";
-import { autoPlay } from "react-swipeable-views-utils";
 
 const baseURL = "http://localhost:3001/artworks";
-const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
 const Artpiece = () => {
   const [artPiece, setArtPiece] = useState(0);
@@ -44,63 +42,6 @@ const Artpiece = () => {
   if (!artPiece) return null;
   
   return(
-    // <Box sx={{ maxWidth: 400, flexGrow: 1 }}>
-    //   <Paper
-    //     square
-    //     elevation={0}
-    //     sx={{
-    //       display: 'flex',
-    //       alignItems: 'center',
-    //       height: 50,
-    //       pl: 2,
-    //       bgcolor: 'background.default',
-    //     }}
-    //   >
-    //     <Typography>{artPiece[activeStep].title}</Typography>
-    //   </Paper>
-    //   <Box
-    //     component="img"
-    //     sx={{
-    //       height: 255,
-    //       display: 'block',
-    //       maxWidth: 400,
-    //       overflow: 'hidden',
-    //       width: '100%',
-    //     }}
-    //     src={artPiece.image}
-    //     alt={artPiece.title}
-    //   />
-    //   <MobileStepper
-    //     variant="text"
-    //     steps={maxSteps}
-    //     position="static"
-    //     activeStep={activeStep}
-    //     nextButton={
-    //       <Button
-    //         size="small"
-    //         onClick={handleNext}
-    //         disabled={activeStep === maxSteps - 1}
-    //       >
-    //         Next
-    //         {theme.direction === 'rtl' ? (
-    //           <KeyboardArrowLeft />
-    //         ) : (
-    //           <KeyboardArrowRight />
-    //         )}
-    //       </Button>
-    //     }
-    //     backButton={
-    //       <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
-    //         {theme.direction === 'rtl' ? (
-    //           <KeyboardArrowRight />
-    //         ) : (
-    //           <KeyboardArrowLeft />
-    //         )}
-    //         Back
-    //       </Button>
-    //     }
-    //   />
-    // </Box>
     <Box sx={{ maxWidth: 400, flexGrow: 1 }}>
       <Paper
         square
@@ -109,13 +50,12 @@ const Artpiece = () => {
           display: 'flex',
           alignItems: 'center',
           height: 50,
-          pl: 2,
           bgcolor: 'background.default',
         }}
       >
         <Typography>{artPiece[activeStep].title}</Typography>
       </Paper>
-      <AutoPlaySwipeableViews
+      <SwipeableViews
         axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
         index={activeStep}
         onChangeIndex={handleStepChange}
@@ -139,7 +79,7 @@ const Artpiece = () => {
             ) : null}
           </div>
         ))}
-      </AutoPlaySwipeableViews>
+      </SwipeableViews>
       <MobileStepper
         steps={maxSteps}
         position="static"
