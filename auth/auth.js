c435cf5a85ac190c2be77e6fe49c12430c691f55
jennifergoldberg <jagoldbe@gmail.com>
2021-12-30 15:05:20 -0500

    auth user login

--- auth/auth.js
+++ auth/auth.js
@@ -21,3 +21,32 @@ passport.use(
     }
   )
 );
+
+// Handles User Login
+passport.use(
+  'login',
+  new localStrategy(
+    {
+      usernameField: "email",
+      passwordField: "password",
+    },
+    async (email, password, done) => {
+      try {
+        const user = await User.findOne({ email });
+
+        if(!user) {
+          return done(null, false, { message: "The email or password you entered is incorrect" });
+        }
+
+        const validate = await user.isValidPassword(password);
+
+        if(!validate) {
+          return done(null, false, { message: "The email or password you entered is incorrect" });
+        }
+        return done(null, user, { message: "Login Successful" });
+      } catch (error) {
+        return done(error);
+      }
+    }
+  )
+);
