4a02e1c3091d67129075b6b66d589b5974272b37
jennifergoldberg <jagoldbe@gmail.com>
2021-12-30 12:10:09 -0500

    created config file and seperated db connection into dbj.hs

---
+++ config/db.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const connection =  async () => {
+  try {
+    const connect = await mongoose.connect(process.env.ATLAS_URI)
+    console.log(`MongoDB connection established successfully! ${connect.connection.host}`)
+  } catch (err) {
+    console.log(`Connection unsuccessful, ${err}`)
+  }
+};
+
+module.exports = connection;
