bd976aa604e24323a4876069e43db6cdd560a185
lonnalu <lonnalu@gmail.com>
2021-12-30 12:04:07 -0800

    Adds slider for artworks

--- client/src/components/Artpiece.js
+++ client/src/components/Artpiece.js
@@ -1,32 +1,175 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
+import { useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import MobileStepper from "@mui/material/MobileStepper";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
+import SwipeableViews from "react-swipeable-views";
+import { autoPlay } from "react-swipeable-views-utils";
 
-const baseURL = "http://localhost:3001/artworks"
+const baseURL = "http://localhost:3001/artworks";
+const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
 const Artpiece = () => {
-  const [artPiece, setArtPiece] = useState(null);
+  const [artPiece, setArtPiece] = useState(0);
 
+  const [activeStep, setActiveStep] = useState(0);
+  const theme = useTheme();
+  
   useEffect(() => {
     axios.get(baseURL).then((response) => {
       setArtPiece(response.data);
     });
   }, [])
+  
+  const maxSteps = artPiece.length;
+  
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleStepChange = (step) => {
+    setActiveStep(step);
+  };
+
 
   if (!artPiece) return null;
-
-  console.log(artPiece);
   
   return(
-    <div className="works__artpiece">
-      {artPiece.map((piece) => {
-        return (
-          <>
-            <h2>{piece.title}</h2>
-            <img src={piece.image} alt={piece.title} />
-          </>
-        )
-      })}
-    </div>
+    // <Box sx={{ maxWidth: 400, flexGrow: 1 }}>
+    //   <Paper
+    //     square
+    //     elevation={0}
+    //     sx={{
+    //       display: 'flex',
+    //       alignItems: 'center',
+    //       height: 50,
+    //       pl: 2,
+    //       bgcolor: 'background.default',
+    //     }}
+    //   >
+    //     <Typography>{artPiece[activeStep].title}</Typography>
+    //   </Paper>
+    //   <Box
+    //     component="img"
+    //     sx={{
+    //       height: 255,
+    //       display: 'block',
+    //       maxWidth: 400,
+    //       overflow: 'hidden',
+    //       width: '100%',
+    //     }}
+    //     src={artPiece.image}
+    //     alt={artPiece.title}
+    //   />
+    //   <MobileStepper
+    //     variant="text"
+    //     steps={maxSteps}
+    //     position="static"
+    //     activeStep={activeStep}
+    //     nextButton={
+    //       <Button
+    //         size="small"
+    //         onClick={handleNext}
+    //         disabled={activeStep === maxSteps - 1}
+    //       >
+    //         Next
+    //         {theme.direction === 'rtl' ? (
+    //           <KeyboardArrowLeft />
+    //         ) : (
+    //           <KeyboardArrowRight />
+    //         )}
+    //       </Button>
+    //     }
+    //     backButton={
+    //       <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+    //         {theme.direction === 'rtl' ? (
+    //           <KeyboardArrowRight />
+    //         ) : (
+    //           <KeyboardArrowLeft />
+    //         )}
+    //         Back
+    //       </Button>
+    //     }
+    //   />
+    // </Box>
+    <Box sx={{ maxWidth: 400, flexGrow: 1 }}>
+      <Paper
+        square
+        elevation={0}
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          height: 50,
+          pl: 2,
+          bgcolor: 'background.default',
+        }}
+      >
+        <Typography>{artPiece[activeStep].title}</Typography>
+      </Paper>
+      <AutoPlaySwipeableViews
+        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+        index={activeStep}
+        onChangeIndex={handleStepChange}
+        enableMouseEvents
+      >
+        {artPiece.map((step, index) => (
+          <div key={step.title}>
+            {Math.abs(activeStep - index) <= 2 ? (
+              <Box
+                component="img"
+                sx={{
+                  height: 255,
+                  display: 'block',
+                  maxWidth: 400,
+                  overflow: 'hidden',
+                  width: '100%',
+                }}
+                src={step.image}
+                alt={step.title}
+              />
+            ) : null}
+          </div>
+        ))}
+      </AutoPlaySwipeableViews>
+      <MobileStepper
+        steps={maxSteps}
+        position="static"
+        activeStep={activeStep}
+        nextButton={
+          <Button
+            size="small"
+            onClick={handleNext}
+            disabled={activeStep === maxSteps - 1}
+          >
+            Next
+            {theme.direction === 'rtl' ? (
+              <KeyboardArrowLeft />
+            ) : (
+              <KeyboardArrowRight />
+            )}
+          </Button>
+        }
+        backButton={
+          <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+            {theme.direction === 'rtl' ? (
+              <KeyboardArrowRight />
+            ) : (
+              <KeyboardArrowLeft />
+            )}
+            Back
+          </Button>
+        }
+      />
+    </Box>
   )
 }
 
