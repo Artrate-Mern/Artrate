36c8c42be58da6bbcbcaa41a2cae49dc1844aabb
lonnalu <lonnalu@gmail.com>
2021-12-29 22:22:26 -0800

    Adds components and changes file structure for frontend

---
+++ client/src/pages/LandingPage/Landing.js
@@ -0,0 +1,11 @@
+import Header from "../../components/Header";
+
+const Landing = () => {
+  return(
+    <div id='landing'>
+      <Header title='Welcome to Artrate' />
+    </div>
+  )
+}
+
+export default Landing;
