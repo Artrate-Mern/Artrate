dd5c3b5a771b501052f6dedc561f895d08f0135f
lonnalu <lonnalu@gmail.com>
2022-01-01 20:24:55 -0800

    Adds responsive nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,13 @@ import Landing from "./pages/LandingPage/Landing";
 import Artworks from "./pages/ArtworksPage/Artworks";
 import PostArt from "./pages/PostArtPage/PostArt"
 import SignUp from "./pages/SignUpPage/SignUp";
+import NavBar from "./components/NavBar";
 import "./App.css";
 
 function App() {
   return (
     <>
+      <NavBar />
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/works" element={<Artworks />} />
---
+++ client/src/components/NavBarData.js
@@ -0,0 +1,18 @@
+import HomeIcon from '@mui/icons-material/Home';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import PersonIcon from '@mui/icons-material/Person';
+
+export const NavBarData = [
+  {
+    path: "/",
+    icon: <PersonIcon />
+  },
+  {
+    path: "/post",
+    icon: <CloudUploadIcon />
+  },
+  {
+    path: "/works",
+    icon: <HomeIcon />
+  }
+]
