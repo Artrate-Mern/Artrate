44318aeca348c7eac91de95e9dc5fe1f2e71eec2
java80 <iekedede@hotmail.com>
2022-01-02 09:43:57 -0500

    Image upload working

---
+++ client/src/components/ArtList.js
@@ -0,0 +1,24 @@
+import React,{useState,useEffect} from 'react';
+import axios from 'axios';
+const baseURL = "http://localhost:3001/artworks";
+const ArtList = (props) => {
+  const [arts, setArts] = useState([]);
+  useEffect(() => {
+    
+    axios.get(baseURL)
+      .then(res => setArts(res.data))
+  }, []);
+  return (
+    <div>
+      <div className="Art-list">
+        {arts.map((art, index) => (
+          <div key={index}>
+            {art.title} | {art.image}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ArtList;
